8da1eddc6ba2491b63cdb136dd1df7fa09697973
kourosh1998 <kourosh1193@gmail.com>
2016-09-04 15:33:18 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,7 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '255184207:AAFYEkjKEU8YFEN4Y6pF_47IHsdcItsBSa8
+
+');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
@@ -115,7 +117,7 @@ function processMessage($message) {
   // process incoming message
   $boolean = file_get_contents('booleans.txt');
   $booleans= explode("\n",$boolean);
-  $admin = **ADMIN**;
+  $admin = 255184207;
   $message_id = $message['message_id'];
   $rpto = $message['reply_to_message']['forward_from']['id'];
   $chat_id = $message['chat']['id'];
